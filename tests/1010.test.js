844ad63ed7cbbc44c9e91a72b4534e357a75a70f
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-30 11:00:25 -0300

    fix: challenge 1010 desc

--- tests/1010.test.js
+++ tests/1010.test.js
@@ -6,7 +6,7 @@ describe("Default - Cálculo Simples", () => {
     ["13 2 15.30", "161 4 5.20", 51.4],
     ["1 1 15.10", "2 1 15.10", 30.2],
   ])(
-    "should return the salary and number of an employee by giving his number, working hours and gains per hour in a string like 'NUMBER = {number}\nSALARY = U$ {salary}'",
+    "Deve ser retornado o valor a ser pago, somando dois casos de peças contendo: código, quantidade de peças e preço uintário de cada peça",
     (piece01, piece02, expected) => {
       const result = challenge1010([piece01, piece02]);
 
